fef7476acedae4d2aec754a904a2edd034e52b55
YesICanCode24 <82227754+YesICanCode24@users.noreply.github.com>
2021-06-28 09:27:09 -0400

    Update Smartphone.java

--- Smartphone.java
+++ Smartphone.java
@@ -1,19 +1,14 @@
-public class Smartphone {
-        public static String manufacturer;    // company
-        public static String model;            // specific model
-        public static String ShowCell() {
-                return manufacturer + " | " + model;
-        }
+class Smartphone {
+   double price;
+   String brand;
+   String model;
+   String phoneType;
+   String color;
+  Boolean fiveG;
+   
+ 
 
-
-        /*String operatingSys;	// operating system android, ios, linux...
-        int memory;			// memory in GB
-        String color;			// primary color
-        int camera_lenses;		// number of camera lenses
-        double price;			// how much does it cost*/
-        public Smartphone(String manufacturer, String model/*, String operatingSys,
-                                        int memory, String color, int camera_lenses, double price*/) {
-            this.manufacturer = manufacturer;
-            this.model = model;
-        }
 }
+   }
+
+
